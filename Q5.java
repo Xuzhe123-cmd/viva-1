efd3c7b529c355e566f88cd003e8b8058541c020
Hong Zhang Lim <54992455+HongZhangLim@users.noreply.github.com>
2025-11-15 01:57:44 +0800

    Add Q5 Golden Harmony solution.

---
+++ Q5.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Q5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of words: ");
+        int T = sc.nextInt(); // number of words
+
+        System.out.println("Enter the words:");
+        
+        for (int t = 0; t < T; t++) {
+            String word = sc.next();
+            String vowels = "aeiou";
+            char[] letters = word.toCharArray();
+            boolean isHarmony = true;
+
+            // Rule 1: Last letter cannot be a vowel
+            // indexOf returns -1 if the character is not found
+            if (vowels.indexOf(letters[letters.length - 1]) != -1) {
+                isHarmony = false;
+            }
+
+            // Rule 2: No two consecutive vowels
+            // Iterate through the word and check consecutive vowels
+            for (int i = 0; i < letters.length - 1; i++) {
+                if (vowels.indexOf(letters[i]) != -1 &&
+                    vowels.indexOf(letters[i + 1]) != -1) {
+                    isHarmony = false;
+                    break;
+                }
+            }
+
+            if (isHarmony) {
+                System.out.println("Harmony");
+            } else {
+                System.out.println("Chaos");
+            }
+        }
+
+        sc.close();
+    }
+}
