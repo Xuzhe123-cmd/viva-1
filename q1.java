f66297a3f63ead5071da88012e38947cd8ad5ec5
LEE MING DAO <25006825@siswa.um.edu.my>
2025-11-15 10:14:28 +0800

    Add files via upload

---
+++ q1.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class q1 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int q;
+
+        System.out.println("Please enter the number of inqueries:");
+        q = input.nextInt();
+
+        for (int i = 1; i <= q; i++) {
+            int a, b, n;
+            System.out.printf(
+                    "Please enther the %d quiries in the format: [Initial Value] [Multiplier Seed] [Charm Length]\n",
+                    i);
+            a = input.nextInt();
+            b = input.nextInt();
+            n = input.nextInt();
+
+            int ans;
+
+            for (int j = 1; j <= n; j++) {
+                ans = (int) (a + (b * (Math.pow(2, j - 1))));
+                System.out.print(ans + " ");
+            }
+            System.out.println();
+        }
+        input.close();
+    }
+}
