c32103790c3cc779ccd40f0babd0ef32e3a488b2
jitesh1059 <25006745@siswa.um.edu.my>
2025-11-16 19:23:57 +0800

    update q3
    fixed the problem statement

--- Q3.java
+++ Q3.java
@@ -1,19 +1,17 @@
 /*
-Your program must be able to generate both of Puan Norah's patterns. Based on the given
-Style code, you will execute the correct logic to print the corresponding pattern for the
-specified Height.
- For Style 'A' (Angled):
-o This is the same as the original "Number Staircase" problem.
-o For a height H, you will print H rows.
-o Row i (where i is from 1 to H) contains the digit i repeated i times.
- For Style 'P' (Pyramid):
-o For a height H, you will print H rows.
-o Each row must be padded with leading spaces to be centered. A pyramid of
-height H has a base width of (2×H)−1.
-o Row i (where i is from 1 to H) will consist of:
-1. A number of leading spaces.
-2. Numbers ascending from 1 up to i.
-3. Numbers descending from i−1 back down to 1.
+The problem requires us to write a program that reads an integer T, the number of test cases. For each test case, read an integer H (Height) and a character S (Style).
+Based on the style S, print the corresponding pattern of height H:
+Style 'A' (Angled): Print H rows. Row i (from 1 to H) must contain the digit i repeated i times.
+Example (H=3):
+1
+22
+333
+Style 'P' (Pyramid): Print H rows. Row i must be a centered, palindromic number sequence counting from 1 up to i and back down to 1.
+Example (H=3):
+     1
+   1 2 1
+1 2 3 2 1
+
  */
 // package q3;
 
@@ -32,6 +30,8 @@ public class Q3 {
         // Receive the number of queries (q) first
         System.out.print("Please input the number of queries: ");
         int q = input.nextInt();
+        
+        System.out.println("Please input the height of the pattern and the style (A or P) separated by spaces: ");
 
         // Repeat for every number of query
         for (int i = 0; i < q; i++) {
@@ -73,6 +73,8 @@ public class Q3 {
                 }
                 
             }
+            System.out.println();
         }
-    }   
+    }
+
 }
